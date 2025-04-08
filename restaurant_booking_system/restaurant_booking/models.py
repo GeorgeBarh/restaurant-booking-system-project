c986710c8a5ccedb0fe738a2a42db8fb5b4477f5
George Barh <georgios.barchampas@students.codeinstitute.net>
2025-04-08 10:07:49 +0000

    crate restaurant model

--- restaurant_booking_system/restaurant_booking/models.py
+++ restaurant_booking_system/restaurant_booking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.name
