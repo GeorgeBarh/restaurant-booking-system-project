209a937cf537341565e20d46ad7d9e6991e26be2
GeorgeBarh <georgebarhabas@gmail.com>
2025-06-28 14:04:24 +0200

    add templates and css

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import MenuListView
 
 urlpatterns = [
-    path('menu/', MenuListView.as_view(), name='menu'),
+    path('', MenuListView.as_view(), name='home'),       # Homepage shows menu
+    path('menu/', MenuListView.as_view(), name='menu'),  # Dedicated menu page
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
+from django.shortcuts import render
 from .models import MenuItem
 
 class MenuListView(generic.ListView):
     model = MenuItem
-    queryset = MenuItem.objects.filter(status=1).order_by('name')  # Only available items
-    template_name = 'menu_list.html'
+    queryset = MenuItem.objects.filter(status=1).order_by('-created_on')
+    template_name = "reservations/index.html"  # Updated path
+    context_object_name = 'menu_list'
+    paginate_by = 6 
