4449d2db22b2e7422c48e14725963895108444c6
GeorgeBarh <georgebarhabas@gmail.com>
2025-06-28 12:27:43 +0200

    Add menu list view with template and URL routing

--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import MenuItem
 
-
-def my_reservation(request):
-    return HttpResponse("Hello, reservation!")
+class MenuListView(generic.ListView):
+    model = MenuItem
+    queryset = MenuItem.objects.all()
+    template_name = "reservations/menu_list.html"
--- restaurant_bookings/urls.py
+++ restaurant_bookings/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from reservations.views import my_reservation
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('reservations/', my_reservation, name='reservation'),
-
+    path("admin/", admin.site.urls),
+    path("", include("reservations.urls"), name="reservations-urls"),
 ]
